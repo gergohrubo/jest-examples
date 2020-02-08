0c07c289bbc3d39a9f31ef2859358ce735167d10
gergohrubo <gergohrubo@gmail.com>
2020-02-08 16:50:10 +0100

    Async await syntax added

--- functions.test.js
+++ functions.test.js
@@ -43,3 +43,10 @@ test('User fetched name should be Leanne Graham', () => {
       expect(data.name).toEqual('Leanne Graham')
     })
 })
+
+//async await syntax
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1)
+  const data = await functions.fetchUser()
+  expect(data.name).toEqual('Leanne Graham')
+})
